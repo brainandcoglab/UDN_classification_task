9ad666b531a15cb0d71b89d71bbaa9f54195bd17
John Salamon <sala0065@flinders.edu.au>
2021-10-18 08:42:28 +1030

    Set a more realistic N trials

--- src/para.js
+++ src/para.js
@@ -16,8 +16,8 @@ import * as band from "./band.js"
 export const N_LINES = 4; // How many lines/band define a ship
 export const N_NOISE = 2; // How many lines/band will be random noise on normal trials
 export const P_CATCH = 0.1; // Proportion of catch trials
-export const N_PRACTICE_TRIALS = 4; // How many trials total of friend + foe
-export const N_TRIALS = 4; // How many trials total of high/lowlight (not including catch trials)
+export const N_PRACTICE_TRIALS = 10; // How many trials total of friend + foe
+export const N_TRIALS = 30; // How many trials total of high/lowlight (not including catch trials)
 
 export const PRACTICE_SIGNATURES = gen.signatures(N_LINES);
 export const VESSEL_SIGNATURES = gen.signatures(N_LINES);
